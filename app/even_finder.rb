dee0c7e339d9bfa4424878061600f8fc425eb177
Dan Briechle <danbriechle@hotmail.com>
2023-12-11 09:30:46 -0700

    Feat: implement even finder

--- app/even_finder.rb
+++ app/even_finder.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class EvenFinder
+  def initialize(arry)
+    @arry = arry
+  end
+
+  def find_even
+    @arry.map { |obj| coerce(obj) }
+         .compact
+         .select(&:even?)
+  end
+
+  def coerce(obj)
+    obj.to_i == 0 ? nil : obj.to_i
+  rescue NoMethodError
+    nil
+  end
 end
