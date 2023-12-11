9ee719c5b7c9ef9ae86b597e4d5f968113ae02b6
Dan Briechle <danbriechle@hotmail.com>
2023-12-11 09:30:15 -0700

    Test: create happy and sad path tests

--- test/even_finder_test.rb
+++ test/even_finder_test.rb
@@ -6,6 +6,38 @@ require_relative '../app/even_finder'
 
 class EvenFinderTest < Minitest::Test
   def test_it_exists
-    assert_instance_of(EvenFinder, EvenFinder.new)
+    assert_instance_of(EvenFinder, EvenFinder.new([]))
+  end
+
+  def test_it_can_find_all_the_even_numbers
+    arry = [1, 2, 36, 29]
+
+    assert_equal([2, 36], EvenFinder.new(arry).find_even)
+  end
+
+  # sad path
+
+  def test_it_can_coerce
+    arry = [1, "2", 36, 29]
+
+    assert_equal([2, 36], EvenFinder.new(arry).find_even)
+  end
+
+  def test_it_handles_no_method_errors
+    arry = [1, "2", 36, Array]
+
+    assert_equal([2, 36], EvenFinder.new(arry).find_even)
+  end
+
+  def test_it_handles_strings
+    arry = [1, "2", "Thirty Six", Array]
+
+    assert_equal([2], EvenFinder.new(arry).find_even)
+  end
+
+  def test_it_handles_nil
+    arry = [1, nil, "Thirty Six", Array]
+
+    assert_equal([], EvenFinder.new(arry).find_even)
   end
 end
